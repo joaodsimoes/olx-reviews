75ad46ae130aea761ac71b90aff6fd8712ce9654
Joao Simoes <jdl.simoes@campus.fct.unl.pt>
2020-10-22 20:05:32 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global chrome */
+import React, { useState, useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Rating } from '@material-ui/lab';
+import axios from 'axios';
+import 'fontsource-roboto';
+
+const addReview = 'https://serene-shelf-97342.herokuapp.com/api/votes'
+const getReviews = 'https://serene-shelf-97342.herokuapp.com/api/votes/username/'
+const Titulo = () => {
+  return (
+    <Typography variant="h6" component="h6">
+      OLX Reviews
+    </Typography>
+  )
+}
+
+const Info = ({ message, info }) => {
+  return (
+    <div>
+      <Typography variant="subtitle2" component="subtitle2">
+        {message}: <strong>{info}</strong>
+      </Typography>
+    </div>
+  )
+}
+
+const UserRating = ({ stars }) => {
+  return (
+    <div style={{ margin: "auto" }}>
+      <Typography variant="subtitle2" component="subtitle2">
+        Score:
+      </Typography>
+      <Rating name="read-only" value={stars} size="small" precision={0.25} readOnly />
+    </div>
+  )
+
+}
+
+const Message = ({ message }) => <p style={{ textAlign: "center" }}>{message}</p>
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [hasVoted, setVoted] = useState(false);
+  const [message, setMessage] = useState('Deixa a tua review!');
+  const [username, setUsername] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [totalVotes, setTotalVotes] = useState(null);
+
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const url = JSON.stringify(tabs[0].url);
+      const currentUser = url.split('/')[5];
+      setUsername(currentUser);
+      axios.get(getReviews+currentUser)
+      .then((reviews) => {
+        const reviewArray = reviews.data;
+        let sum = 0;
+        reviewArray.forEach((review) => sum+=review.rating)
+        setTotalVotes(reviewArray.length);
+        const denominator = reviewArray.length === 0 ? 1: reviewArray.length; //to avoid dividing by zero
+        setRating(sum/denominator);
+      })
+
+    });
+    
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Titulo />
+      <Info message={"Username"} info={username} />
+      <Info message={"Total de votos"} info={totalVotes} />
+      <UserRating stars={rating} />
+      <Message message={message} />
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={1}
+        onChange={(event, newValue) => {
+          if (!hasVoted) {
+            setValue(newValue);
+            const voteObject = { username, rating: newValue }
+            axios
+              .post(addReview, voteObject)
+              .then(response => {
+                setVoted(true);
+                const newRating = ((rating*totalVotes)+newValue)/(totalVotes+1)
+                setRating(newRating);
+                setTotalVotes(totalVotes + 1);
+                setMessage(`Votaste com ${newValue} estrelas!`);
+                setTimeout(() => setMessage('Obrigado por votares!'), 3000);
+              }).catch(error => setMessage('Já votaste neste utilizador!'))
+
+          }
+        }
+        }/>
     </div>
+
   );
 }
 
