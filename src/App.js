3b6e73b6cdad2c1d5a43f81f1b921fbfad7c836b
Joao Simoes <jdl.simoes@campus.fct.unl.pt>
2020-10-26 19:01:42 +0100

    updated UI
    - Now uses conditional rendering to render the reviews

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import { Banner } from 'material-ui-banner';
 import React, { useState, useEffect } from 'react';
-import { TextField, Typography, Button, List,ListItem,Divider,ListItemText } from '@material-ui/core';
+import { TextField, Typography, Button, List, ListItem, Divider, ListItemText } from '@material-ui/core';
 import { Rating } from '@material-ui/lab';
 import axios from 'axios';
 import 'fontsource-roboto';
@@ -31,7 +31,7 @@ const UserRating = ({ stars }) => {
       </Typography>
       <div
         style={{
-          position: 'relative', left: '52%', top: '12px',
+          position: 'relative', left: '66%', top: '12px',
           transform: 'translate(-50%, -50%)',
         }}
       >
@@ -50,40 +50,90 @@ const Message = ({ message }) => {
   )
 }
 
-const ShowReviews= ({reviews,showReviews}) => {
-  if(showReviews){
-    return(
-      <div style = {{width:'350px'}}>
-      <List>
-        <Divider/>
-        {reviews.map((e) =><><ListItem><ListItemText primaryTypographyProps= {{variant:"subtitle2", component:"subtitle2"}} primary={e.review+" - "+e.rating+"/5"}/></ListItem><Divider/></>)}
-      </List>
+const ShowReviews = ({ reviews, showReviews }) => {
+  if (showReviews) {
+    return (
+      <div style={{ width: '350px' }}>
+        <List>
+          {reviews.map((e) => <><ListItem><ListItemText primaryTypographyProps={{ variant: "subtitle2", component: "subtitle2" }} primary={e.review + " - " + e.rating + "/5"} /></ListItem><Divider /></>)}
+        </List>
       </div>
     )
   }
   return null;
 }
 
-const Reviews = ({ reviews }) => {
-  const [showReviews, setShowReviews] = useState(false);
+const Reviews = ({ reviews, showReviews, setShowReviews }) => {
   const iconComponent = showReviews ? <VisibilityOffIcon size='small' /> : <VisibilityIcon size='small' />;
 
   return (
-    <div style={{ marginTop: "10px",display: 'grid', placeItems: 'center' }}>
-      <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        className="reviewButton"
-        startIcon={iconComponent}
-        onClick = {()=> setShowReviews(!showReviews)}
-      >
-        Reviews
+    <div style={{ display: 'grid', placeItems: 'center' }}>
+
+      <ShowReviews reviews={reviews} showReviews={showReviews} />
+      <div style={{marginTop:"7px"}}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          className="reviewButton"
+          startIcon={iconComponent}
+          onClick={() => setShowReviews(!showReviews)}
+        >
+          Reviews
   </Button>
-  <ShowReviews reviews = {reviews} showReviews= {showReviews}/>
+      </div>
     </div>)
 
-  }
+}
+
+const WriteReview = (props) => {
+  return (
+    <div>
+      <div>
+        <br />
+        <Info message={"Username"} info={props.username} />
+        <Info message={"Total de votos"} info={props.totalVotes} />
+        <UserRating stars={props.rating} />
+        <Message message={props.message} />
+        <Rating
+          name="hover-feedback"
+          value={props.value}
+          precision={1}
+          onChange={(event, newValue) => {
+            if (!props.hasVoted && props.review.length > 5) {
+              props.setValue(newValue);
+              const voteObject = { username: props.username, rating: newValue, review: props.review }
+              props.setReview('');
+              axios
+                .post(addReview, voteObject)
+                .then(response => {
+                  props.setVoted(true);
+                  const newRating = ((props.rating * props.totalVotes) + newValue) / (props.totalVotes + 1)
+                  props.setRating(newRating);
+                  props.setTotalVotes(props.totalVotes + 1);
+                  props.setMessage(`Votaste com ${newValue} estrelas!`);
+                  setTimeout(() => props.setMessage('Obrigado por votares!'), 3000);
+                }).catch(error => props.setMessage('Já votaste neste utilizador!'))
+
+            } else {
+              props.setMessage(`Por favor escreve uma review.`);
+              setTimeout(() => props.setMessage('Deixa a tua review!'), 2000);
+
+            }
+          }
+          } />
+        <TextField value={props.review} id="outlined-basic" label="Review" variant="outlined" multiline rowsMax={6} onChange={(event) => {
+          if (event.target.value.length < 100) {
+            props.setReview(event.target.value)
+          }
+        }
+        } />
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
   const [value, setValue] = useState(0);
   const [hasVoted, setVoted] = useState(false);
@@ -92,7 +142,8 @@ function App() {
   const [rating, setRating] = useState(0);
   const [totalVotes, setTotalVotes] = useState(null);
   const [review, setReview] = useState('');
-  const[reviews,setReviews] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [showReviews, setShowReviews] = useState(false);
 
   console.log(review)
 
@@ -116,7 +167,21 @@ function App() {
 
 
   }, []);
-
+  const wrProps = {
+    username,
+    totalVotes,
+    rating,
+    message,
+    value,
+    hasVoted,
+    review,
+    setValue,
+    setReview,
+    setRating,
+    setVoted,
+    setTotalVotes,
+    setMessage
+  }
   return (
     <div>
       <Banner
@@ -125,48 +190,8 @@ function App() {
         open
         showDismissButton={false}
       />
-      <div style = {{display: 'grid', placeItems: 'center'}}>
-      <Info message={"Username"} info={username} />
-      <Info message={"Total de votos"} info={totalVotes} />
-      <UserRating stars={rating} />
-      <Message message={message} />
-      <Rating
-        name="hover-feedback"
-        value={value}
-        precision={1}
-        onChange={(event, newValue) => {
-          if (!hasVoted && review.length > 5) {
-            setValue(newValue);
-            const voteObject = { username, rating: newValue, review }
-            setReview('');
-            axios
-              .post(addReview, voteObject)
-              .then(response => {
-                setVoted(true);
-                const newRating = ((rating * totalVotes) + newValue) / (totalVotes + 1)
-                setRating(newRating);
-                setTotalVotes(totalVotes + 1);
-                setMessage(`Votaste com ${newValue} estrelas!`);
-                setTimeout(() => setMessage('Obrigado por votares!'), 3000);
-              }).catch(error => setMessage('Já votaste neste utilizador!'))
-
-          } else {
-            setMessage(`Por favor escreve uma review.`);
-            setTimeout(() => setMessage('Deixa a tua review!'), 2000);
-
-          }
-        }
-        } />
-      <TextField value={review} id="outlined-basic" label="Review" variant="outlined" multiline rowsMax={6} onChange={(event) => {
-        if (event.target.value.length < 100) {
-          setReview(event.target.value)
-        }
-      }
-      } />
-      </div>
-      <Reviews reviews={reviews} />
-      
-
+      {!showReviews && <WriteReview {...wrProps} />}
+      <Reviews reviews={reviews} showReviews={showReviews} setShowReviews={setShowReviews} />
     </div>
 
   );
